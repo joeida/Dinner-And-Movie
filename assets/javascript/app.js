1fdb15038512db393c0e792ee5a139a9c87df69a
joe <joeida@jpl.nasa.gov>
2016-08-20 20:42:26 -0700

    Added Address Search Capability

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,28 @@
 
 var compute = {
 
-    getZip: function() {
+    getAddress: function() {
+        var addressSentence = $('#addressInput').val().trim();
+        var addressList = addressSentence.split(' ');
+        var city = $('#cityInput').val().trim();
+        var state = $('#stateInput').val().trim();
+        var addressObj = {
+            addressList: addressList,
+            city: city,
+            state: state
+        }
+        return addressObj;
+    },
+
+    checkZip: function() {
         var zip = $('#zipInput').val().trim();
         var zipValid = /^\d{5}$/;
-
         if (zip.match(zipValid)) { 
             console.log('valid zip input');
             return zip;
         } else {
             console.log('invalid zip input'); 
         } 
-
     },
 
     getSearchCriteria: function() {
@@ -27,9 +38,26 @@ var compute = {
         }
     },
 
-    getGeo: function (zip, searchCriteria) {
-        var query = zip;
-        var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + zip + "&key=AIzaSyBZgPjyk5ho6Axhr_2dU1Ay3M7rU71HXvs";
+    getGeo: function (addressObj, searchCriteria) {
+        var query = '';
+        var queryList = [];
+        var addressList = addressObj.addressList;
+        var city = addressObj.city;
+        var state = addressObj.state;
+        if (addressList) {
+            var addressQuery = addressList.join('+');
+            queryList.push(addressQuery);
+        }
+        if (city) {
+            queryList.push(city);
+        }
+        if (state) {
+            queryList.push(state);
+        }
+        query = queryList.join(',+');
+        console.log(query);
+        var queryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + query + "&key=AIzaSyBZgPjyk5ho6Axhr_2dU1Ay3M7rU71HXvs";
+        console.log(queryURL);
         $.ajax({url: queryURL, method: 'GET'})
         .then(function(response) {
             var lat = response.results[0].geometry.location.lat;
@@ -43,6 +71,16 @@ var compute = {
                 var city = addressList[0];
                 var state = addressList[1];
                 var country = addressList[2];
+                console.log('city: ' + city);
+                console.log('state: ' + state);
+                console.log('country: ' + country);
+            }
+            if (addressList.length === 4) {
+                var street = addressList[0];
+                var city = addressList[1];
+                var state = addressList[2];
+                var country = addressList[3];
+                console.log('street: ' + street);
                 console.log('city: ' + city);
                 console.log('state: ' + state);
                 console.log('country: ' + country);
@@ -66,28 +104,35 @@ var compute = {
     getRest: function(geoObj) {
         var lat = geoObj.lat;
         var lng = geoObj.lng;
-        var searchCriteria = geoObj.searchCriteria;
-        if (geoObj.page === 1) {
-            var queryURL = "https://developers.zomato.com/api/v2.1/search?lat=" + lat + "&lon=" + lng + "&radius=1000&sort=" + searchCriteria + "&apikey=4e48375b934f553b68f4409de5bdf9bb";
-        } else if (geoObj.page === 2) {
-            var queryURL = "https://developers.zomato.com/api/v2.1/search?start=21&lat=" + lat + "&lon=" + lng + "&radius=1000&sort=" + searchCriteria + "&apikey=4e48375b934f553b68f4409de5bdf9bb";
+        var searchCriteria;
+        if (geoObj.searchCriteria) {
+            searchCriteria = geoObj.searchCriteria;
         } else {
-            var queryURL = "https://developers.zomato.com/api/v2.1/search?lat=" + lat + "&lon=" + lng + "&radius=1000&sort=" + searchCriteria + "&apikey=4e48375b934f553b68f4409de5bdf9bb";
+            searchCriteria = 'rating';
         }
+        console.log(lat, lng, searchCriteria);
+        // if (geoObj.page === 2) {
+        //     var queryURL = "https://developers.zomato.com/api/v2.1/search?start=21&lat=" + lat + "&lon=" + lng + "&radius=1000&sort=" + searchCriteria + "&apikey=4e48375b934f553b68f4409de5bdf9bb";
+        // }
+        var queryURL = "https://developers.zomato.com/api/v2.1/search?lat=" + lat + "&lon=" + lng + "&radius=1000&sort=" + searchCriteria + "&apikey=4e48375b934f553b68f4409de5bdf9bb";
         $.ajax({url: queryURL, method: 'GET'})
         .then(function(response) {
             console.log(response);
             var name;
             var location;
             var cuisine;
+            var rating;
+            var priceRange;
             var link;
             console.log(response.restaurants.length);
             for (var i = 0; i < response.restaurants.length; i++) {
                 name = response.restaurants[i].restaurant.name;
                 location = response.restaurants[i].restaurant.location.address;
                 cuisine = response.restaurants[i].restaurant.cuisines;
-                link = response.restaurants[i].restaurant.events_url;
-                render.displayRest(name, location, cuisine, link);
+                rating = response.restaurants[i].restaurant.user_rating.aggregate_rating;
+                priceRange = response.restaurants[i].restaurant.price_range;
+                link = response.restaurants[i].restaurant.url;
+                render.displayRest(name, location, cuisine, rating, priceRange, link);
             }
         });
     }
@@ -96,16 +141,21 @@ var compute = {
 
 var render = {
 
-    displayRest: function(name, location, cuisine, link) {
+    displayRest: function(name, location, cuisine, rating, priceRange, link) {
         var nameP = $('<p>');
         var locationP = $('<p>');
         var cuisineP = $('<p>');
+        var ratingP = $('<p>');
+        var priceRangeP = $('<p>');
         var linkA = $('<a>');
         var choiceBtn = $('<button>');
+        var blankP = $('<p>');
         choiceBtn.addClass('restaurant btn btn-success');
         nameP.text('Restaurant: ' + name);
         locationP.text('Location: ' + location);
         cuisineP.text('Cuisine: ' + cuisine);
+        ratingP.text('User Rating: ' + rating);
+        priceRangeP.text('Price Range: ' + priceRange);
         var linkGlyph = $('<span>');
         linkGlyph.addClass('glyphicon glyphicon-info-sign');
         linkA.attr('href', link);
@@ -115,7 +165,10 @@ var render = {
         $('#restaurantTable').append(nameP);
         $('#restaurantTable').append(locationP);
         $('#restaurantTable').append(cuisineP);
+        $('#restaurantTable').append(ratingP);
+        $('#restaurantTable').append(priceRangeP);
         $('#restaurantTable').append(linkA);
+        $('#restaurantTable').append(blankP);
     },
 
     displayRestMap: function(origin, destination) {
@@ -141,11 +194,11 @@ var render = {
 
 $('#getRestBtn').on('click', function() {
 
-    var zip = compute.getZip();
+    var addressObj = compute.getAddress();
     var searchCriteria = compute.getSearchCriteria();
+    console.log(addressObj);
     console.log(searchCriteria);
-    compute.getGeo(zip, searchCriteria);
-    console.log(zip);
+    compute.getGeo(addressObj, searchCriteria);
 
     return false;
 
