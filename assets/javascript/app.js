4db230ac158053b190acc796d10e38f3f0f9f285
joe <joeida@jpl.nasa.gov>
2016-08-23 12:15:19 -0700

    Rearranged html links in test html for test purposes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -512,32 +512,4 @@ $(document).ready(function() {
 
     });
 
-// // SELECT
-//     $('select').material_select();
-
-// // CAROUSEL
-//     $('.carousel').carousel();
-//     $('.carousel-slider').slider({full_width: true});
-
-//  // MODAL
-//     $('.modal-trigger').leanModal();
 });
-          
-// $('#modal1').openModal();
-// $('#modal1').closeModal();
-
-// // SMOOTH SCROLLING
-// $(function() {
-//     $('a[href*="#"]:not([href="#"])').click(function() {
-//         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-//             var target = $(this.hash);
-//             target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//         if (target.length) {
-//             $('html, body').animate({
-//             scrollTop: target.offset().top
-//                 }, 1000);
-//             return false;
-//             }
-//         }
-//     });
-// });
