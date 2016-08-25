e9630e569f02097cd32c18f267b19edffaa05c38
Eric Zuckerman <ericzuckerman@Erics-MBP.att.net>
2016-08-25 07:45:40 -0700

    Fixed address finder. Movie search is totally functional.

--- assets/javascript/movieapp.js
+++ assets/javascript/movieapp.js
@@ -8,17 +8,28 @@ var config = {
 
 var database = firebase.database();
 
+//Creating needed global variables.
+var theaterAddress = "";
+var showtime = "";
+var movieTitle = "";
+var movieTime = "";
+var movieTheater = "";
+var movieDate = "";
 
+//API keys for OnConnect.
 apiKey1 = "w2v7bscpkzmezeny47ueqsau"
 apiKey2 = "93dvq9k3hx7ahh997jb5tyd2"
 apiKey3 = "9vu3zjqxjwg49mm9p72mqjau"
+apiKey4 = "adf7jebmw23v6yjr6f6qcqsf"
 
+//When enter zip, find movies, clear database, empty anything that was in movieCntainer.
+//Hide unneeded buttons and containers.
 $('#zipInput').on('click', function(){
 	var movieZip = $('#zipCode').val().trim();
 	database.ref('/movieOptions').remove();
-	$('#zipCode').val('');
 	$("#movieContainer").empty();
 	findMovie(movieZip);
+	$('#zipCode').val('');
 	$("#backbutton1").hide();
 	$("#backbutton2").hide();
 	$("#movieContainer").show();
@@ -27,6 +38,8 @@ $('#zipInput').on('click', function(){
 	return false;
 });
 
+//When click on movie choice, find showtimes, empty anything that was in showtimeContainer.
+//Hide unneeded buttons and containers.
 $("#movieContainer").on("click", ".movie", function(){
 	var movie = $(this).data(movie);
 	$("#showtimeContainer").empty();
@@ -38,8 +51,16 @@ $("#movieContainer").on("click", ".movie", function(){
 	$("#selectionContainer").hide();
 });
 
+//When click on showtime choice, find address of theater, empty selectionContainer.
+//Hide unneeded buttons and containers.
 $("#showtimeContainer").on("click", ".showtime", function(){
 	var showtime = $(this).data(showtime);
+	database.ref('/movieChoice').set({
+				title: showtime.title,
+				date: showtime.date,
+				time: showtime.time,
+				theater: showtime.theater
+			});	
 	$("#selectionContainer").empty();
 	findAddress(showtime);
 	$("#backbutton1").hide();
@@ -49,6 +70,7 @@ $("#showtimeContainer").on("click", ".showtime", function(){
 	$("#selectionContainer").show();
 });
 
+//Back button that allows you to pick a different movie.
 $("#backbutton1").on("click", function(){
 	$("#backbutton1").hide();
 	$("#backbutton2").hide();
@@ -57,6 +79,7 @@ $("#backbutton1").on("click", function(){
 	$("#selectionContainer").hide();
 });
 
+//Back button that allows you to pick a different showtime.
 $("#backbutton2").on("click", function(){
 	$("#backbutton1").show();
 	$("#backbutton2").hide();
@@ -65,11 +88,8 @@ $("#backbutton2").on("click", function(){
 	$("#selectionContainer").hide();
 });
 
-function displayMovie() {
-	var movieBlock = $("<div>");
-	$("#movieContainer").append("<h3>" + movie + "</h3>" + theater + "<br>" + date + "<br>" + time + "<br>");
-};
-
+//Take zip code and query OnConnect for movies playing nearby. Store movie info in each button.
+//Disply movie info. Also get showtimes for each movie for later display.
 function findMovie(movieZip){
 	var date = moment().format("YYYY-MM-DD");
 	var onConnectQueryURL = "https://data.tmsapi.com/v1.1/movies/showings?startDate=" + date + "&zip=" + movieZip + "&api_key=" + apiKey1;
@@ -81,7 +101,6 @@ function findMovie(movieZip){
 			var movieTitle;
 			if (response[i].title){
 				movieTitle = response[i].title;
-				//findPoster(movieTitle);
 			}
 			else {
 				movieTitle = "Untitled";
@@ -136,28 +155,28 @@ function findMovie(movieZip){
 				if (response[i].showtimes[j]) {
 					showtime = response[i].showtimes[j].dateTime;
 					var dateTimeArray = showtime.split("T");
-					var date = dateTimeArray[0];
+					var movieDate = dateTimeArray[0];
 					var time = dateTimeArray[1];
 					var movieTime = moment(time, 'hh:mm').format("h:mm a");
 				}
 				else {
 					showtime = "No Showtime";
-					date = "No Date";
-					time = "No Showtime";
+					movieDate = "No Date";
+					movieTime = "No Showtime";
 				};
-				var theater;
+				var movieTheater;
 				if (response[i].showtimes[j]) {
-					theater = response[i].showtimes[j].theatre.name;
+					movieTheater = response[i].showtimes[j].theatre.name;
 				}
 				else {
-					theater = "No Theater";
+					movieTheater = "No Theater";
 				};
 
 				database.ref('/movieOptions').push({
 					title: movieTitle,
-					date: date,
+					date: movieDate,
 					time: movieTime,
-					theater: theater
+					theater: movieTheater
 				});
 			};
 		};
@@ -168,52 +187,68 @@ function findShowtimes(movie){
 	var data = database.ref('/movieOptions');
 	data.on("child_added", function(snapshot){
 		var snapshot = snapshot.val();
-		var movieTitle = snapshot.title;
-		var date = snapshot.date;
-		date = moment(date, "YYYY-MM-DD").format("dddd, MMMM DD, YYYY");
-		var time = snapshot.time;
-		var time = time
-		var theater = snapshot.theater;
+		movieTitle = snapshot.title;
+		movieDate = snapshot.date;
+		movieDate = moment(movieDate, "YYYY-MM-DD").format("dddd, MMMM DD, YYYY");
+		movieTime = snapshot.time;
+		movieTheater = snapshot.theater;
 		if (movieTitle == movie.title){
-			var showtimeBlock = $("<h3>" + movieTitle + "</h3>" + date + "<br>" + time + "<br>" + theater + "</br>");
+			var showtimeBlock = $("<h3>" + movieTitle + "</h3>" + movieDate + "<br>" + movieTime + "<br>" + movieTheater + "</br>");
 			var showtimeButton = $("<button>");
 			showtimeButton.text("Select Showtime");
 			showtimeButton.addClass("showtime");
 			showtimeButton.attr("data-title", movieTitle);
-			showtimeButton.attr("data-theater", theater);
-			showtimeButton.attr("data-date", date);
-			showtimeButton.attr("data-time", time);
+			showtimeButton.attr("data-theater", movieTheater);
+			showtimeButton.attr("data-date", movieDate);
+			showtimeButton.attr("data-time", movieTime);
 			$("#showtimeContainer").append(showtimeBlock);
 			$("#showtimeContainer").append(showtimeButton);
 			findPoster(movieTitle);
-		}; 
+		};
 	});						
 };
+function findAddress(showtime){	
 
-function findAddress(showtime){
-	var title = showtime.title;
-	var date = showtime.date;
-	var time = showtime.time
-	var theater = showtime.theater;
+  	var losfeliz = new google.maps.LatLng(34.1063,-118.2848);
+	var map = new google.maps.Map(document.getElementById('mapdiv'), {
+     	//center: losfeliz,
+    });
 
-	var googleQueryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + theater + "&type=movie_theater&key=AIzaSyBCXM-2P4McxM6KX4Nr4dURfIYOVSUKhPo";
+	var request = {
+    	// location: losfeliz,
+    	// radius: '500',
+    	query: showtime.theater,
+    	type: 'movie_theater'
+  	};
+  	var service = new google.maps.places.PlacesService($('#mapdiv').get(0));
+	service.textSearch(request, callbackAddress);
+};
+		
+function callbackAddress(results, status) {
+  	if (status == google.maps.places.PlacesServiceStatus.OK) {
+    	theaterAddress = results[0].formatted_address;
+    	displaySelection(theaterAddress);
+    };
+ };
 
-	$.ajax({
-		url : googleQueryURL,
-		method : 'GET'
-	})
-	.done(function(response){
-		var theaterAddress = response.results[0].formatted_address;
-		var selectionBlock = $("<h3>" + title + "</h3>" + date + "<br>" + time + "<br>" + theater + "<br>" + theaterAddress + "<br>");
-		$("#selectionContainer").append(selectionBlock);
-		$("#showtimeContainer").hide();
-		database.ref('/movieChoice').set({
-					title: title,
-					date: date,
-					time: time,
-					theater: theater,
-					address: theaterAddress
-		});
+ function displaySelection(){
+ 	var choice = database.ref('/movieChoice');
+	choice.on("value", function(snapshot){
+		var choiceSnapshot = snapshot.val();
+		movieTitle = choiceSnapshot.title;
+		movieDate = choiceSnapshot.date;
+		movieTime = choiceSnapshot.time;
+		movieTheater = choiceSnapshot.theater;	
+ 		var selectionBlock = $("<h3>" + movieTitle + "</h3>" + movieDate + "<br>" + movieTime + "<br>" + movieTheater + "<br>" + theaterAddress + "<br>");
+		$("#selectionContainer").html(selectionBlock);
+ 		$("#showtimeContainer").hide();
+    	database.ref('/movieChoice').set({
+			title: movieTitle,
+			date: movieDate,
+			time: movieTime,
+			theater: movieTheater,
+			address: theaterAddress
+		});  	
 	});
 };
 
